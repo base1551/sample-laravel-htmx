a37c360c9a96a8d3a12b8005a4981b7d89f59545
Oka <baseball.1.kyoto@gmail.com>
2024-02-04 17:23:07 +0900

    Add alcoholic beverage seed data

--- database/seeders/AlcoholicBeverageSeeder.php
+++ database/seeders/AlcoholicBeverageSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\AlcoholicBeverage;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,25 @@ class AlcoholicBeverageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $alcoholic_beverages = [
+            ['name' => 'ビール', 'alcoholContent' => '4%～6%'],
+            ['name' => 'ワイン', 'alcoholContent' => '9%～16%'],
+            ['name' => 'シャンパン', 'alcoholContent' => '8%～12%'],
+            ['name' => '日本酒', 'alcoholContent' => '15%～17%'],
+            ['name' => 'ウイスキー', 'alcoholContent' => '40%～50%'],
+            ['name' => 'バーボン', 'alcoholContent' => '40%～50%'],
+            ['name' => 'ラム', 'alcoholContent' => '37.5%～80%'],
+            ['name' => 'テキーラ', 'alcoholContent' => '35%～55%'],
+            ['name' => 'ジン', 'alcoholContent' => '37.5%～47%']
+        ];
+
+        foreach($alcoholic_beverages as $alcoholic_beverage) {
+
+            AlcoholicBeverage::create([
+                'name' => $alcoholic_beverage['name'],
+                'alcohol_content' => $alcoholic_beverage['alcoholContent']
+            ]);
+
+        }
     }
 }
